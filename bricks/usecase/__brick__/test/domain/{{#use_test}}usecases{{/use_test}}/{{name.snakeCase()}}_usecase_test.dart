2fb7033c6fc17b0ae6b68261bead987ed899fe80
Ananta <ananta.tyastama@mekari.com>
2023-03-21 16:42:01 +0700

    add unit test option for usecase
    fix typo
    
    fix typo part 2
    
    fix typo part 3
    
    fix typo part 44
    
    fix typo part 5
    
    fix typo part 6

---
+++ bricks/usecase/__brick__/test/domain/{{#use_test}}usecases{{/use_test}}/{{name.snakeCase()}}_usecase_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mockito/mockito.dart';
+
+
+void main() {
+  late {{name.pascalCase()}}UseCase {{name.camelCase()}}UseCase;
+  late {{repo.pascalCase()}}Repository mock{{repo.pascalCase()}}Repository;
+
+  setUp(() {
+    mock{{repo.pascalCase()}}Repository = Mock{{repo.pascalCase()}}Repository();
+    {{name.pascalCase()}}UseCase =
+        {{name.pascalCase()}}UseCase({{repo.snakeCase()}}Repository: mock{{repo.pascalCase()}}Repository);
+  });
+
+  group("{{name.pascalCase()}} UseCase", () {
+    test(
+        'Given {{repo.pascalCase()}}Repository'
+        'When {{repo.pascalCase()}}Repository call {{name.pascalCase()}}'
+        'Then return {{repo.pascalCase()}} detail data', () async {
+      when(mock{{repo.pascalCase()}}Repository.{{name.camelCase()}}(request: request))
+          .thenAnswer((_) async => const Right(successResponse));
+
+      final result = await {{name.camelCase()}}UseCase.call(request);
+
+      expect(result, const Right(successResponse));
+    });
+
+    test(
+        'Given {{repo.pascalCase()}}Repository'
+        'When {{repo.pascalCase()}}Repository call {{name.pascalCase()}}'
+        'Then return failure', () async {
+      when(mock{{repo.pascalCase()}}Repository.{{name.camelCase()}}(request: request))
+          .thenAnswer((_) async => Left(failedResponse));
+
+      final result = await {{name.camelCase()}}UseCase.call(request);
+
+      expect(result, Left(failedResponse));
+    });
+  });
+}
